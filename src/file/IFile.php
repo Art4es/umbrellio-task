bc34fcab0d63bebb3fe4422f96d62601d5be6464
Art4es <Art4es@gmail.com>
2019-09-17 16:24:47 +0300

    Change typehint in phpdoc style

--- src/file/IFile.php
+++ src/file/IFile.php
@@ -4,8 +4,15 @@
 namespace Art4es\file;
 
 
+use Art4es\exceptions\FileNotFoundException;
+
 interface IFile
 {
+    /**
+     * @param string $mode
+     * @return resource
+     * @throws FileNotFoundException
+     */
     public function open(string $mode = 'r');
 
     public function close(): bool;
