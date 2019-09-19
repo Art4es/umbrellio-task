7329f74948c95f3cb50ed00ef77b5605be458348
Art4es <Art4es@gmail.com>
2019-09-19 16:04:40 +0300

    Add tests for common Finder's behavior

--- src/Finder.php
+++ src/Finder.php
@@ -5,21 +5,44 @@ namespace Art4es;
 
 
 use Art4es\file\IFile;
+use Art4es\file\validators\FileExistingValidator;
+use Art4es\file\validators\IValidator;
 use Art4es\strategies\IStrategy;
 
 class Finder implements IFinder
 {
     private $file;
     private $strategy;
+    /** @var IValidator[] */
+    private $validators = [];
 
     public function __construct(IFile $file, IStrategy $strategy)
     {
         $this->file = $file;
         $this->strategy = $strategy;
+        array_push($this->validators, new FileExistingValidator());
     }
 
     public function execute()
     {
+        $this->validate();
         return $this->strategy->search($this->file);
+    }
+
+    private function validate()
+    {
+        foreach ($this->validators as $validator) {
+            $validator->validate($this->file);
+        }
+    }
+
+    public function setFile(IFile $newFile)
+    {
+        $this->file = $newFile;
+    }
+
+    public function setStrategy(IStrategy $newStrategy)
+    {
+        $this->strategy = $newStrategy;
     }
 }
---
+++ tests/FinderTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Art4es\Tests\files;
+
+
+use Art4es\exceptions\validator\FileDoesNotExistsException;
+use Art4es\file\File;
+use Art4es\Finder;
+use Art4es\strategies\SubstringSearchingStrategy;
+use PHPUnit\Framework\TestCase;
+
+class FinderTest extends TestCase
+{
+    public function testFileExistingValidator()
+    {
+        $file = new File('notExistedFile');
+        $strategy = new SubstringSearchingStrategy('empty');
+        $finder = new Finder($file, $strategy);
+        $this->expectException(FileDoesNotExistsException::class);
+        $finder->execute();
+    }
+
+    public function testValidatorDoesNotBreakCommonBehavior()
+    {
+        $file = new File(__DIR__ . '/files/test2.txt');
+        $strategy = new SubstringSearchingStrategy('7');
+        $finder = new Finder($file, $strategy);
+        $result = $finder->execute();
+        $expected = [
+            ['line' => 3, 'position' => 1]
+        ];
+        $this->assertEquals($expected, $result);
+    }
+}
